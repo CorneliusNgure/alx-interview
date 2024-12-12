a577aea8e0fc1f8d467e9ceba29f8b6516a58d8b
CorneliusNgure <kingcornelius07@gmail.com>
2024-12-12 14:37:14 -0800

    Island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,29 +1,54 @@
 #!/usr/bin/python3
-""" Module to calculate perimeter of the island described in a grid. """
+"""Island Perimeter"""
 
 
-def island_perimeter(grid: list[list[int]]) -> int:
-    """
-    Calculate the perimeter of the island described in grid.
+def check_val(x):
+    """_summary_
 
     Args:
-        grid (list[list[int]]): Rectangular grid: 0 = water and 1 = land.
+        n (_type_): _description_
 
     Returns:
-        int: The perimeter of the island.
+        _type_: _description_
     """
-    perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
+    if (x == 0):
+        return 1
+    return 0
 
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 1:
-                perimeter += 4
 
-                if r > 0 and grid[r - 1][c] == 1:
-                    perimeter -= 2
-                if c > 0 and grid[r][c - 1] == 1:
-                    perimeter -= 2
+def island_perimeter(grid):
+    """_summary_
 
-    return perimeter
+    Args:
+        grid (_type_): _description_
+    """
+
+    row = len(grid)
+    col = len(grid[0])
+    assert (1 <= row and col <= 100), "length must be between 1 an 100"
+
+    x = 0
+    for i in range(row):
+        for j in range(col):
+            assert (grid[i][j] == 0) or (grid[i][j] == 1), \
+                "grid numbers must be 0 or 1"
+            if grid[i][j] == 1:
+                if i-1 < 0:
+                    x += 1
+                else:
+                    x += check_val(grid[i-1][j])
+                if j-1 < 0:
+                    x += 1
+                else:
+                    x += check_val(grid[i][j-1])
+
+                try:
+                    x += check_val(grid[i+1][j])
+                except IndexError:
+                    x += 1
+                try:
+                    x += check_val(grid[i][j+1])
+                except IndexError:
+                    x += 1
+
+    return x
